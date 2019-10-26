1773e38e60efc1f9ea1d3464fb3dc42e5e0d02d1
AlzoPalzo <ali@ALI-Laptop.localdomain>
2019-10-26 17:13:08 +0100

    creates a state upon reading file input

--- src/RoboVac.js
+++ src/RoboVac.js
@@ -6,10 +6,37 @@ state={
     gridHeight: 0,
     vacPosX: 0,
     vacPosY: 0,
-    dirt: []
+    dirt: [],
+    dirtCount: 0,
 }
 
-fs.readFileSync('./textFiles/example1.txt', 'utf8', function(err, data){
-    if (err) throw err;
-    input = data;
-})
+function readFile(){
+    try {
+        input = fs.readFileSync("./textFiles/example1.txt", "utf8").split('\n')
+    } catch (error) {
+        console.log('ERROR: ', error.stack)
+    }
+}
+
+function initialState(){
+    for (let i = 0; i < input.length - 1; i++) {
+        console.log(i)
+        const xyVals = input[i].split(' ')
+        if (i === 0) {
+            state.gridLength = xyVals[0]
+            state.gridHeight = xyVals[1]
+        }
+        else if(i === 1){
+            state.vacPosX = xyVals[0]
+            state.vacPosY = xyVals[1]
+        }
+        else{
+            state.dirt = [...state.dirt, xyVals]
+        }
+    }
+}
+readFile()
+initialState()
+
+console.log(input)
+console.log(state)
